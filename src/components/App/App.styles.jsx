7718987da999874f37eb19227a95ced61531cc9c
K0nfy <27099419+K0nfy@users.noreply.github.com>
2021-10-08 15:57:24 +0300

    Deploy fix

--- src/components/App/App.styles.jsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const StyledApp = styled.div`
-  width: 320px;
-  margin: auto;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
--- src/components/PlayButton/PlayButton.jsx
+++
@@ -1,5 +0,0 @@
-import { Button } from './PlayButton.styles';
-
-export default function PlayButton({ isSquare }) {
-  return (<Button isSquare={isSquare}></Button>);
-}
